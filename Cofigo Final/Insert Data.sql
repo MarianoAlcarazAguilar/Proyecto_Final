ae7fbc6ee488f28c7eb3d266071ef7a49f35760c
joseluistl <66537882+joseluistl@users.noreply.github.com>
2021-12-07 23:23:55 -0600

    Create Insert Data.sql

---
+++ Cofigo Final/Insert Data.sql
@@ -0,0 +1,57 @@
+insert into area_estudio (descripcion) values 
+('Ciencias Puras'), 
+('Humanidades y Arte'),
+('Ingenierias'),
+('Ciencias Sociales y de la Comunicacion'),
+('Salud y Medicina');
+
+insert into dispositivos (nombre) values
+('Teléfono inteligente'),
+('Computadora de escritorio'),
+('Computadora portátil'),
+('Tablet'),
+('Consola de Videojuegos'),
+('Otro'),
+('Ninguno');
+
+insert into escalares_web (pregunta) values
+('Confianza en ingresar NOMBRE'),
+('Confianza en ingresar CORREO'),
+('Confianza en ingresar NÚMERO DE TELÉFONO'),
+('Confianza en ingresar TARJETA DE CRÉDITO'),
+('Confianza en ingresar FOTOGRAFÍA DE UNA IDENTIFICACIÓN'),
+('Confianza en ingresar DIRECCIÓN'),
+('Confianza en ingresar FECHA DE NACIMIENTO'),
+('INFORMADO SOBRE EL USO de tu información ingresada'),
+('Confianza en que harán BUEN USO DE TU INFORMACIÓN'),
+('De acuerdo con COMPARTIR CON TERCEROS'),
+('Frecuencia de LEER TÉRMINOS Y CONDICIONES'),
+('Frecuencia de ACEPTAR TÉRMINOS Y CONDICIONES');
+
+insert into escalares_social_media (pregunta) values 
+('Preocupación USO INFORMACIÓN en redes sociales');
+
+insert into datos (descripcion) values 
+('Nombre'),
+('Correo'),
+('Número de Teléfono'),
+('Edad'),
+('Dirección'),
+('Tarjeta de Crédito'),
+('Fotografía de una ID'),
+('Fecha de nacimiento'),
+('Ninguno'),
+('Otro');
+
+insert into escalares_e_commerce (descripcion) values 
+('Seguridad compra NO FRAUDE'),
+('Seguridad información NO ROBADA'),
+('Seguridad datos TRATADOS RESPONSABLEMENTE');
+
+insert into forma_pago (nombre) values 
+('Tarjeta de crédito o débito'),
+('Transferencia bancaria'),
+('PayPal o ApplePay'),
+('Tarjeta digital'),
+('Efectivo en puntos de pago'),
+('Otro');
